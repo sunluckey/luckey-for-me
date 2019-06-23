fe9fd4837d58cc4fdafbda3f4ddc9179b64ca56c
sunluckey <48814179+sunluckey@users.noreply.github.com>
2019-06-24 00:27:43 +0800

    gg
    gg

---
+++ meizitu/meizitu/spiders/fuli.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from bs4 import BeautifulSoup
+from ..items import MeizituItem
+import time
+import random
+
+class FuliSpider(scrapy.Spider):
+    name = 'fuli'
+    allowed_domains = ['https://www.mzitu.com']
+    start_urls = ['https://www.mzitu.com/185752']
+    user_agent_list = [
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1"
+        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+    ]
+
+    def start_url(self):
+        ua = random.choice(self.user_agent_list)  # 随机抽取User-Agent
+        headers = {
+            'Accept-Encoding': 'gzip, deflate, sdch, br',
+            'Accept-Language': 'zh-CN,zh;q=0.8',
+            'Connection': 'keep-alive',
+            'Referer': 'https://www.mzitu.com/',
+            'User-Agent': ua
+        }  # 构造请求头
+        yield scrapy.Request(self.start_urls, callback=self.parse, headers=headers)
+
+    def parse(self, response):
+        bs = BeautifulSoup(response.text)
+        div_gg = bs.find_all('div', attrs={'class': 'main-image'})
+        item = MeizituItem()
+        img_urls = []
+        for i in div_gg:
+            # x = i['src']
+            img_url = i.img.get('src')
+            next_url = i.a.get('href')
+            img_title = i.img.get('alt')
+            # img_url = i.next.next.next.attrs['src']
+            # img_title = i.next.next.next.attrs['alt']
+            # next_url = i.next.next.attrs['href']
+            img_urls.append(img_url)
+            item['url'] = img_urls
+            item['title'] = img_title
+            yield item
+            time.sleep(2)
+            yield scrapy.Request(url=next_url, callback=self.parse)
+            # print(x)
